3417078037eb41b605a854939a58d572bf49f262
Adrienn Timko <adrienn.timko@gmail.com>
2019-03-13 12:40:02 +0100

    updated

--- ajax.js
+++ ajax.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 
+    $("#search").keypress(function(e){
+        console.log(e);
+    });
+
     $("#search").on("click", function(){
         var searchTerm = $("#searchTerm").val();
         var url = "http://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
@@ -8,8 +12,12 @@ $(document).ready(function(){
             type: "GET",
             dataType: "json",
             data: function(data, status, jqXHR){
-                console.log(data)
-            },
+                console.log(data);
+                $("#output"),html();
+                for(var i=0; i<data[1].length;i++){
+                    $(".output").prepend("<div><div class="infos"><a href="+data[3][i]+"><h2>" + data[1][i]+ "</h2>" + "<p>" + data[2][i] + "</p></a></div></div>");
+                }
+            }
         })
         .done(function() {
             console.log("success");
@@ -25,6 +33,9 @@ $(document).ready(function(){
 });
 
 /*
+API Sandbox:
+https://en.wikipedia.org/wiki/Special:ApiSandbox#action=query&list=search&srsearch=Craig%20Noone&format=jsonfm
+
 GET Request
 To search for page titles matching Craig Noone GET Request would be:
 
